04abd7f107ce99190dfaa315464be5948bb72341
Jack <jackmichaelsimons59@gmail.com>
2018-03-02 12:39:37 +0000

    Initial Commit :+1:

---
+++ ul_encryption/DLLEntry.cs
@@ -0,0 +1,34 @@
+﻿using RGiesecke.DllExport;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ul_encryption
+{
+    public class DLLEntry
+    {
+        // This 2 line are IMPORTANT and if changed will stop everything working
+        // To send a string back to ARMA append to the output StringBuilder, ARMA outputSize limit applies!
+        [DllExport("_RVExtension@12", CallingConvention = System.Runtime.InteropServices.CallingConvention.Winapi)]
+        public static void RVExtension(StringBuilder output, int outputSize, [MarshalAs(UnmanagedType.LPStr)] string function)
+        {
+            outputSize--;
+            output.Append(sha256(function));
+            return;
+        }
+        static string sha256(string randomString)
+        {
+            var crypt = new System.Security.Cryptography.SHA256Managed();
+            var hash = new System.Text.StringBuilder();
+            byte[] crypto = crypt.ComputeHash(Encoding.UTF8.GetBytes(randomString));
+            foreach (byte theByte in crypto)
+            {
+                hash.Append(theByte.ToString("x2"));
+            }
+            return hash.ToString();
+        }
+    }
+}
